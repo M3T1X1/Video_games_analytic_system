79bbaf74f7c69b001ed13bf0c02989544a98defa
M3T1X1 <dusza2003@protonmail.com>
2025-10-12 15:48:31 +0200

    still testing stuff

--- video_games/settings.py
+++ video_games/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend', 'static'),
 ]
-# assasa
+# assasa SADASA
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
